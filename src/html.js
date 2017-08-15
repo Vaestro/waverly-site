19fd57709ac06ef3a9f1fcba8ac4dce134522b91
Edgar Li <edgarli@Edgars-MBP.fios-router.home>
2017-08-15 09:05:20 -0400

    slick css from html.js

--- src/html.js
+++ src/html.js
@@ -24,8 +24,6 @@ export default class HTML extends React.Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"/>
                     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet"/>
-                    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
-                    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
                     {this.props.headComponents}
                     {css}
                 </head>
