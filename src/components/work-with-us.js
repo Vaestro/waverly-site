9214ff1a2c2ebdaf8ce8efd57de04c1014eaa87d
EricSong8612 <js8612@nyu.edu>
2017-08-15 17:08:12 -0400

    case-study-component-in-index

--- src/components/work-with-us.js
+++ src/components/work-with-us.js
@@ -7,7 +7,6 @@ export default (props) => {
   return(
     <div>
       <Grid columns={2} stackable>
-
         <Grid.Row className="grid-row">
           <Grid.Column >
             <h3 style={{
@@ -16,15 +15,18 @@ export default (props) => {
               fontWeight: '700',
               marginBottom: 10
             }}>Interested in working with us?</h3>
-
-            <a id="typeform-button" className="typeform-share button" href="https://dopeatlas.typeform.com/to/lyD8J5" data-mode="drawer_left" target="_blank">Get in touch today
+            <a
+              id="typeform-button"
+              className="typeform-share button"
+              href="https://dopeatlas.typeform.com/to/lyD8J5"
+              data-mode="drawer_left"
+              target="_blank">
+              Get in touch today
             </a>
-
             <script dangerouslySetInnerHTML={{
               __html: ` (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()`
             }}/>
           </Grid.Column>
-
           <Grid.Column></Grid.Column>
         </Grid.Row>
       </Grid>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import {Image, Button, Divider, Grid} from 'semantic-ui-react'
 import '../css/particle-styles.css';
 import '../css/master.css';
 
+import CaseStudyIndex from '../components/case-study-index';
+import WorkWithUs from '../components/work-with-us';
+
 export default class Index extends React.Component {
   componentDidMount() {
     if (window) {
@@ -46,73 +49,30 @@ export default class Index extends React.Component {
 
           <div id="particles-header"></div>
         </div>
+
         <Grid columns={2} stackable style={{
           backgroundColor: 'white',
           marginTop: '150px'
         }}>
           <Grid.Row className="grid-row">
-            <Grid.Column >
-              <div>
-                <div className="projectContent">
-                  <Image src="https://s3.amazonaws.com/pearlventures/aware_screenshot.png" id="iphone"/>
-
-                  <h3 style={{
-                    color: '#00C7FF',
-                    fontSize: '20px',
-                    fontWeight: '700',
-                    marginBottom: 10
-                  }}>Aware</h3>
-                  <p style={{
-                    fontSize: '28px',
-                    marginBottom: 10,
-                    fontWeight: '400'
-                  }}>Challenge your friends for charity</p>
-                  <p>We were asked to bring life to a business model. Meet Aware, the app that allows you to challenge your friends for charity that we built and designed from scratch.</p>
-                  <Link style={{
-                    fontSize: '20px',
-                    fontWeight: 100,
-                    color: 'black',
-                    textDecoration: 'none',
-                    marginBottom: 10,
-                    fontWeight: '400',
-                    letterSpacing: '-0.28px',
-                    borderBottom: 'solid 5px #00C7FF'
-                  }} to="/aware-case-study/">View case study</Link>
-                </div>
-              </div>
-            </Grid.Column>
-            <Grid.Column>
-
-              <div className="projectContainer">
-                <div className="projectContent">
-
-                  <Image src='https://s3.amazonaws.com/pearlventures/memegod_screenshot.png' id="iphone"/>
-                  <h3 style={{
-                    color: '#00EC7A',
-                    fontSize: '20px',
-                    fontWeight: '700',
-                    marginBottom: 10
-                  }}>Memes Against Friends</h3>
-                  <p style={{
-                    fontSize: '28px',
-                    marginBottom: 10,
-                    fontWeight: '400'
-                  }}>Compete with your friends to make the best memes</p>
-                  <p>Our team considered the ubiquity of memes in our digital world and wanted to experiment with a fun way to get people together to create memes</p>
-                  <Link style={{
-                    fontSize: '20px',
-                    fontWeight: 100,
-                    color: 'black',
-                    textDecoration: 'none',
-                    marginBottom: 10,
-                    fontWeight: '400',
-                    letterSpacing: '-0.28px',
-                    borderBottom: 'solid 5px #00EC7A'
-                  }} to="/memes-against-friends-case-study/">View case study</Link>
-                </div>
-              </div>
-            </Grid.Column>
+            <CaseStudyIndex
+              imgUrl={'https://s3.amazonaws.com/pearlventures/aware_screenshot.png'}
+              title={'Aware'}
+              headline={'Challenge your friends for charity'}
+              description={'We were asked to bring life to a business model. Meet Aware, the app that allows you to challenge your friends for charity that we built and designed from scratch'}
+              to={'/aware-case-study/'}
+              color={'#00C7FF'}
+            />
+            <CaseStudyIndex
+              imgUrl={'https://s3.amazonaws.com/pearlventures/memegod_screenshot.png'}
+              title={'Memes Against Friends'}
+              headline={'Compete with your friends to make the best memes'}
+              description={'Our team considered the ubiquity of memes in our digital world and wanted to experiment with a fun way to get people together to create memes'}
+              to={'/memes-against-friends-case-study/'}
+              color={'#00EC7A'}
+            />
           </Grid.Row>
+
           <Grid.Row>
             <Grid.Column ></Grid.Column>
             <Grid.Column>
@@ -122,7 +82,6 @@ export default class Index extends React.Component {
                 fontWeight: '700',
                 marginBottom: 10
               }}>Our Portfolio</h3>
-
               <Link style={{
                 fontSize: '24px',
                 fontWeight: 100,
@@ -172,33 +131,13 @@ export default class Index extends React.Component {
               }}>Integrate us as your on-demand team</p>
             </Grid.Column>
           </Grid.Row>
+
           <Grid.Row>
             <Image src="https://s3.amazonaws.com/pearlventures/studio%403x.png" id="studio"/>
           </Grid.Row>
-
         </Grid>
-        <Grid columns={2} stackable>
-
-          <Grid.Row className="grid-row">
-            <Grid.Column >
-              <h3 style={{
-                color: '#000',
-                fontSize: '20px',
-                fontWeight: '700',
-                marginBottom: 10
-              }}>Interested in working with us?</h3>
-
-              <a id="typeform-button" className="typeform-share button" href="https://dopeatlas.typeform.com/to/lyD8J5" data-mode="drawer_left" target="_blank">Get in touch today
-              </a>
-
-              <script dangerouslySetInnerHTML={{
-                __html: ` (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()`
-              }}/>
-            </Grid.Column>
-            <Grid.Column></Grid.Column>
-          </Grid.Row>
-        </Grid>
-
+        
+        <WorkWithUs />
       </div>
     )
   }
