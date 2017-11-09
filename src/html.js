aaa40c0dcecb60b5a8e472e618f8df828c1d1303
Edgar <liedgar7@gmail.com>
2017-11-09 18:09:46 -0500

    Changed font to Source Sans Pro

--- src/html.js
+++ src/html.js
@@ -24,11 +24,11 @@ export default class HTML extends React.Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"/>
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-                    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet"/>
+                    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" rel="stylesheet"/>
                     {this.props.headComponents}
                     {css}
                 </head>
-                
+
                 <body>
                     <div id="___gatsby" dangerouslySetInnerHTML={{
                         __html: this.props.body
