c7cfcd563389be8b8dd11bacd5e739ba9b77ee9b
Edgar <liedgar7@gmail.com>
2017-11-11 14:49:29 -0500

    Removed old case studies for now

--- src/pages/durite-case-study.js
+++ src/pages/durite-case-study.js
@@ -1,23 +1,3 @@
 import React from "react"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
-// import {Image, Button, Divider, Grid} from 'semantic-ui-react'
-import '../css/master.css';
-// import Slides from '../components/slides';
-// import Summary from '../components/case-study-summary';
-// import Contributions from '../components/contributions';
-// import WorkWithUs from '../components/work-with-us';
-
-// export default class DuriteCaseStudy extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <Summary title='DURITE' headline='The Best in Terrazo Manufacturing' description='Coming soon.' url='http://duriteusa.com' imgUrl='https://s3.amazonaws.com/pearlventures/durite_screenshot.png' color='#FF0200'/>
-//
-//         <Contributions left={['Strategy', 'Product Roadmapping', 'User Research', 'Design', 'Branding']} right={['Resposive Web Development', 'Software Application Architecture', 'Quality Assurance', 'Analytics & Optimization']}/>
-//
-//         <WorkWithUs/>
-//       </div>
-//     )
-//   }
-// }
